d3bbdac530358ab25eb2ecb043be71be4a140ec2
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-08-21 15:40:15 -0700

    Event handlers added

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function handleUpdate() {
     console.log(this.value);
 }
 inputs.forEach(inputs => addEventListener('change', handleUpdate));
+inputs.forEach(input => addEventListener('mousemove', handleUpdate));
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+

7d6bc60143d2aeac530cde18f3dfd10660ef24ee
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-08-21 15:44:09 -0700

    Final product

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    console.log(this.value);
-}
-inputs.forEach(inputs => addEventListener('change', handleUpdate));
-inputs.forEach(input => addEventListener('mousemove', handleUpdate));
-
-function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
